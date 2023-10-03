6bdff59c1226dbe2fb15345aaa6dca8d76869848
Ian Sze <iansze1124@gmail.com>
2023-10-03 14:58:27 -0400

    intial commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { FC } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import SignOut from "./pages/SignOut";
+import SignIn from "./pages/SignIn";
+import Header from "./components/Header";
+
+const App: FC = () => {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-out" element={<SignOut />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,41 @@
+import { FC } from "react";
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+interface HeaderProps {}
+
+const Header: FC<HeaderProps> = ({}) => {
+  return (
+    <header className=" bg-slate-200 shadow-sm ">
+      <div className="flex justify-between mx-auto w-5/6 items-center p-3">
+        <h1 className="font-bold text-sm sm:text-xl flex-wrap">
+          <span className="text-slate-600">Tim</span>
+          <span className="">Estate</span>
+        </h1>
+        <form className="bg-slate-100 p-2 rounded-lg flex justify-center items-center ">
+          <input
+            type="text"
+            className="bg-transparent outline-none w-24 sm:w-64"
+            placeholder="Search..."
+          />
+          <FaSearch className="text-slate-600" />
+        </form>
+        <ul className="flex gap-4">
+          <Link to="/">
+            <li className="hidden md:inline text-slate-700 hover:underline cursor-pointer">Home</li>
+          </Link>
+          <Link to="about">
+            <li className="hidden md:inline text-slate-700 hover:underline cursor-pointer">
+              About
+            </li>
+          </Link>
+          <Link to="sign-in">
+            <li className=" text-slate-700 hover:underline cursor-pointer">Sign In</li>
+          </Link>
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
