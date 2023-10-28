e5623fb21bc00ec132a717595a9dc5b7c54dddb8
Ian Sze <iansze1124@gmail.com>
2023-10-28 17:29:46 -0400

    add path

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ import cookieParser from "cookie-parser";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
+import path from "path";
 
 dotenv.config();
 
@@ -41,6 +42,11 @@ app.use((req, res, next) => {
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(path.resolve(), "../client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(path.resolve(), "../client/dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
